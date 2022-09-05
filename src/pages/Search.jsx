04516ce596601aca5a4fc65787e7c76adc42c8f5
Rayanderson Sousa <rayandersoncooper@gmail.com>
2022-09-05 17:27:41 -0300

    Requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,9 +1,49 @@
 import React from 'react';
 
 export default class Search extends React.Component {
+  state = {
+    search: '',
+    isDisabled: true,
+  };
+
+  handleChange = (event) => {
+    const { target } = event;
+    this.setState({
+      search: target.value,
+    }, this.handleButton);
+  };
+
+  handleButton = () => {
+    const { search } = this.state;
+    const minChar = 2;
+    if (search.length >= minChar) {
+      this.setState({
+        isDisabled: false,
+      });
+    }
+  };
+
   render() {
+    const { isDisabled, search } = this.state;
     return (
-      <div data-testid="page-search">Busca</div>
+      <div data-testid="page-search">
+        Busca
+        <form>
+          <input
+            data-testid="search-artist-input"
+            type="text"
+            value={ search }
+            onChange={ this.handleChange }
+          />
+          <button
+            data-testid="search-artist-button"
+            type="button"
+            disabled={ isDisabled }
+          >
+            Buscar
+          </button>
+        </form>
+      </div>
     );
   }
 }
