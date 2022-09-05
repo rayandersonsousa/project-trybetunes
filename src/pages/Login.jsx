c0a742f7579d937b7e89206a647884a7dbd408d4
Rayanderson Sousa <rayandersoncooper@gmail.com>
2022-09-05 15:51:07 -0300

    Requisito 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,68 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import Loading from './Loading';
+import { createUser } from '../services/userAPI';
 
 export default class Login extends React.Component {
+  state = {
+    user: '',
+    isDisabled: true,
+    loading: false,
+    isRedirecting: false,
+  };
+
+  handleChange = (event) => {
+    const { target } = event;
+    this.setState({
+      user: target.value,
+    }, this.handleButton);
+  };
+
+  handleButton = () => {
+    const { user } = this.state;
+    const charMin = 3;
+    if (user.length >= charMin) {
+      this.setState({
+        isDisabled: false,
+      });
+    }
+  };
+
+  handleNewUser = async () => {
+    const { user } = this.state;
+    this.setState({
+      loading: true,
+    });
+    await createUser({ name: user });
+    this.setState({
+      isRedirecting: true,
+    });
+  };
+
   render() {
+    const { isDisabled, loading, isRedirecting } = this.state;
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+        {(loading) ? <Loading /> : (
+          <section>
+            <h1>Login</h1>
+            <input
+              type="text"
+              data-testid="login-name-input"
+              onChange={ this.handleChange }
+            />
+            <button
+              type="button"
+              data-testid="login-submit-button"
+              disabled={ isDisabled }
+              onClick={ this.handleNewUser }
+            >
+              Entrar
+            </button>
+          </section>
+        )}
+        {isRedirecting && <Redirect to="/search" />}
+      </div>
     );
   }
 }
